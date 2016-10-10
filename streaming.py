f2ca0ae87ca2ddbd835920a7dc1330dd0f1eb5e7
mattdixon85 <matt_dixon85@yahoo.co.uk>
2016-10-10 23:41:38 +0100

    removed example from bottom of centricity class

--- streaming.py
+++ streaming.py
@@ -72,13 +72,3 @@ class Stream:
             centricity = self.centricity(config)
             centricity.process_lines(lines)
             
-if __name__ == '__main__':
-    config = dict(
-        srcs = ['type 1','type 2','type 3','type 4','type 5','type 6','type 7']
-        ,tsrcs = ['type 1','type 2','type 3','type 4','type 5','type 6','type 7','people']
-        ,wsrcs = ['type 1','type 2','type 3','type 4','type 5','type 6','type 7']
-        ,emit = ['type 1','type 2','type 3','type 4','type 5','type 6','type 7']
-        ,immu = ['people']
-        )
-    stream = Stream(Centricity, config)
-    stream.run(sys.stdin)
